260f2800bcf58faac9b3ff21c33b49c1dc7f7ca2
dao huy tuan <tuandh95@gmail.com>
2025-02-25 14:37:39 +0700

    it

---
+++ LangSwitcher/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LangSwitcher
+//
+//  Created by zyye on 24/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
